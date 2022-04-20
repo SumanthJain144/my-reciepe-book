6980501530d0856bad3b282ed30eae20865d8720
sumanth K srivarma <s.srivarma@devon.nl>
2022-04-20 10:13:21 +0530

    bob added function in the feature 2 and feature 3 of header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,10 +10,14 @@ console.log("alice added this header as part of 1st commit")
 
 feature2:any{
 console.log("alice added this feature1 as part of 2nd commit")
+console.log("this function added by bob in feature 2")
+console.log("this function added by bob in feature 2")
 }
 
 feature3:any{
 console.log("feature 3 added in the master")
+console.log("this function added by bob in feature 3")
+console.log("this function added by bob in feature 3")
 }
 
 
