74292d0d25817a42c6689f5362de08739f639485
sumanth K srivarma <s.srivarma@devon.nl>
2022-04-20 10:15:00 +0530

    alice also changed feature2 and 3 in the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,10 +10,14 @@ console.log("alice added this header as part of 1st commit")
 
 feature2:any{
 console.log("alice added this feature1 as part of 2nd commit")
+console.log("alice added some more functions here for feature 2")
+console.log("alice added some more functions here for feature 2")
 }
 
 feature3:any{
 console.log("feature 3 added in the master")
+console.log("alice added some more functions here for feature 3")
+console.log("alice added some more functions here for feature 3")
 }
 
 
